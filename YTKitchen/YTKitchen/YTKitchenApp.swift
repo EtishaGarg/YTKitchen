d85fbd853c429bef6aef46308294312f2811bc48
Etishagarg <gargetisha@gmail.com>
2025-06-02 01:06:26 +0530

    Initial commit

---
+++ YTKitchen/YTKitchen/YTKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTKitchenApp.swift
+//  YTKitchen
+//
+//  Created by Etisha Garg on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct YTKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
